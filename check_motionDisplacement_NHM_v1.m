b15b3119ed975a0d839206209d78712e33ffebd3
Peng Wang <pengwg@gmail.com>
2024-03-07 13:59:43 -0500

    check motion refactoring

--- check_motionDisplacement_NHM_v1.m
+++ check_motionDisplacement_NHM_v1.m
@@ -8,24 +8,26 @@ function  [Number_of_Frame_To_Remove]= check_motionDisplacement_NHM_v1(path_to_f
 %input are: path of the subjects , name of the output text file and
 %treshold
 
-
-v = importdata([path_to_func ls([path_to_func 'rp_c*'])]);
-cfg.motionparam = [path_to_func ls([path_to_func 'rp_c*'])];
+rp_c_file = dir([path_to_func 'rp_c*']);
+v = importdata([path_to_func rp_c_file.name]);
+cfg.motionparam = [path_to_func rp_c_file.name];
 cfg.prepro_suite =  'spm' ;
 cfg.radius = 50 ; %default value
 
-[fwd,rms]=bramila_framewiseDisplacement(cfg)
-Frame2remove = zeros(length(fwd),1);
-Frame2remove(find(fwd > threshold)) = 1;
+addpath bramila/
+
+[fwd,rms] = bramila_framewiseDisplacement(cfg)
+% Frame2remove = zeros(length(fwd),1);
+% Frame2remove(fwd > threshold) = 1;
 
 Number_of_Frame_To_Remove = length(find(fwd > threshold));
-cd ('C:\Users\natha\Downloads\FUS_transferToNIDA(1)\FUS_transferToNIDA\')
-a = figure(1)
-plot(v(:,1))
+% cd ('C:\Users\natha\Downloads\FUS_transferToNIDA(1)\FUS_transferToNIDA\')
+fig = figure(1);
+plot(v(:, 1))
 hold on
-plot(v(:,2))
+plot(v(:, 2))
 hold on
-plot(v(:,3))
-ylim([-2,2])
-saveas(a,['Figure_' char(subjects) '_' char(session) '.png']);
-close(a)
+plot(v(:, 3))
+ylim([-2, 2])
+saveas(fig, [path_to_func 'Figure_' subjects '_' session '.png']);
+close(fig)
